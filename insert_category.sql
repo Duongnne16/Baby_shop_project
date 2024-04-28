860e2eb8ac9be4b015e6afeef16ecaab83839dfa
Nguyen Duc Dai Duong <daiduong2004bn@gmail.com>
2024-04-28 22:35:08 +0700

    update dbms

---
+++ insert_category.sql
@@ -0,0 +1,13 @@
+﻿USE AngelAndBabyShop
+GO
+
+INSERT INTO [Category] (name) VALUES 
+(N'Đồ bộ bé gái'),
+(N'Váy bé gái'),
+(N'Áo thun bé gái'),
+(N'Áo khoác bé gái'),
+(N'Quần bé gái'),
+(N'Đồ bộ bé trai'),
+(N'Áo thun bé trai'),
+(N'Quần bé trai'),
+(N'Áo khoác bé trai');
